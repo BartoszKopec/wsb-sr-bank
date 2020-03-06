238de379c7c88331573436b753b5ed67e6c03f67
kopec.bartosz.app <kopec.bartosz.app@gmail.com>
2020-03-06 18:47:04 +0100

    dodano implementację BaseApi w WorkerApp

---
+++ WorkerApp/Services/WorkerApi.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Core.Apis;
+using Core.Models;
+using Newtonsoft.Json;
+
+namespace WorkerApp.Services
+{
+    class WorkerApi : BaseApi
+    {
+        public WorkerApi() : base()
+        {
+        }
+
+        public async Task<(bool, string)> InsertAccount(Account account, CancellationToken token) =>
+            await PostAsync("/accounts/insert", JsonConvert.SerializeObject(account), token);
+
+        public async Task<(bool, string)> GetAccountData(int id, CancellationToken token) =>
+            await GetAsync($"/accounts/get/{id}", token);
+        
+        public async Task<(bool, string)> GetAllAccounts(CancellationToken token) =>
+            await GetAsync("/accounts/getall", token);
+        
+        public async Task<(bool, string)> UpdateAccountData(Account account, CancellationToken token) =>
+            await PutAsync("/accounts/update", JsonConvert.SerializeObject(account), token);
+        
+        public async Task<(bool, string)> DeleteAccount(int id, CancellationToken token) =>
+            await DeleteAsync($"/accounts/delete/{id}", token);
+
+
+
+
+    }
+}
